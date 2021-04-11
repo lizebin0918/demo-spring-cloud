fe18ca38a2cb3a51e042cf9fd7cb0056fc97b1f1
lizebin <lizebin0918@gmail.com>
2021-04-11 22:01:44 +0800

    删除eruka依赖

--- eureka/src/main/java/com/lzb/Application.java
+++ eureka/src/main/java/com/lzb/Application.java
@@ -9,6 +9,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
  * @author lizebin
  */
 //@EnableEurekaServer
+//@EnableEurekaClient
 @SpringBootApplication
 public class Application {
 
