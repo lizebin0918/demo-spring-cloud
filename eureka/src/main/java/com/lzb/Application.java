416256e8063b6b83c0804fba9d4ef9826e21b103
lizebin <lizebin0918@gmail.com>
2021-04-11 21:48:22 +0800

    正常启动

--- eureka/src/main/java/com/lzb/Application.java
+++ eureka/src/main/java/com/lzb/Application.java
@@ -1,13 +1,19 @@
 package com.lzb;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
 /**
  * <br/>
  * Created on : 2021-04-11 20:42
  * @author lizebin
  */
+//@EnableEurekaServer
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("123");
+        new SpringApplicationBuilder(Application.class).run(args);
     }
 
 }
