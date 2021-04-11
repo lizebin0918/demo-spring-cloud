9c540dfe460f0daa62cedf6a596ca259124d34d2
lizebin <lizebin0918@gmail.com>
2021-04-11 22:34:28 +0800

    Spring boot 正常启动

--- eureka/src/main/java/com/lzb/Application.java
+++ eureka/src/main/java/com/lzb/Application.java
@@ -9,7 +9,6 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
  * @author lizebin
  */
 //@EnableEurekaServer
-//@EnableEurekaClient
 @SpringBootApplication
 public class Application {
 
