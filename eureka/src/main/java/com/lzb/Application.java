2f17511c5454d69e35439c7ab5035cc251144865
lizebin <lizebin0918@gmail.com>
2021-04-11 20:42:49 +0800

    测试文件

---
+++ eureka/src/main/java/com/lzb/Application.java
@@ -0,0 +1,13 @@
+package com.lzb;
+/**
+ * <br/>
+ * Created on : 2021-04-11 20:42
+ * @author lizebin
+ */
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+
+}
